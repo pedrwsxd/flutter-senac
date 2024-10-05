8f425d6f5d0ee78ee88755137345c503150bc433
Pedro_Henrick <Pedrwsxd@gmail.com>
2024-10-05 14:42:05 -0300

    Atualizacao app feirinha

--- app_feirinha/lib/src/auth/sign_in_screen.dart
+++ app_feirinha/lib/src/auth/sign_in_screen.dart
@@ -1,11 +1,57 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:app_feirinha/src/components/custom_text_field.dart';
+import 'package:app_feirinha/src/home/home_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
 
+  // Função para realizar o login
+  Future<void> loginUser(String email, String senha, BuildContext context) async {
+    final url = Uri.parse('http://localhost:8080/user/login'); // API de login
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'email': email, 'senha': senha}),
+      );
+
+      if (response.statusCode == 200) {
+        // Login bem-sucedido
+        //print('Login bem-sucedido: ${response.body}');
+        // aqui você pode navegar para outra tela ou exibir uma mensagem de sucesso
+        // Navegar para a nova tela HomeScreen
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const HomeScreen()),
+        );
+      } else {
+        // login falhou
+        //print('Erro no login: ${response.body}');
+        // Mostre uma mensagem de erro ao usuário
+        // exibir uma mensagem de erro
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Erro no login: ${response.body}')),
+        );
+      }
+    } catch (e) {
+      //print('Erro ao concectar à API: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro ao conectar à API: $e')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    // definição dos campos para usurario de senha
+    TextEditingController emailController = TextEditingController();
+    TextEditingController senhaController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Entrada"),
@@ -14,8 +60,56 @@ class SignInScreen extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: Container(
-              color: Colors.green,
+            child: Column(
+              // alinhamento vertical
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text.rich(
+                  TextSpan(
+                    style: TextStyle(
+                      fontSize: 40,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'Feir',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'inha',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // categorias
+
+                SizedBox(
+                  height: 50,
+                  child: DefaultTextStyle(
+                    style: const TextStyle(
+                        fontSize: 25,
+                        fontFamily: 'Horizon',
+                        color: Colors.white),
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        RotateAnimatedText('Frutas'),
+                        RotateAnimatedText('Legumes'),
+                        RotateAnimatedText('Verduras'),
+                        RotateAnimatedText('Temperos'),
+                      ],
+                      onTap: () {
+                        print("Tap Event");
+                      },
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           Expanded(
@@ -29,18 +123,81 @@ class SignInScreen extends StatelessWidget {
                   borderRadius: BorderRadius.vertical(
                     top: Radius.circular(45),
                   )),
-              child: const Column(
+              child: Column(
                 children: [
                   //Email
                   CustomTextField(
                     icon: Icons.email,
                     label: 'Email',
-                  ),                 
+                    controller: emailController,
+                  ),
                   //Senha
                   CustomTextField(
                     icon: Icons.lock,
                     label: 'Senha',
-                    isObscure: true,
+                    isSecret: true,
+                    controller: senhaController,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18),
+                        ),
+                      ),
+                      onPressed: () {
+                        // Comparação de dados
+                        String email = emailController.text;
+                        String senha = senhaController.text;
+                        loginUser(email, senha, context);
+                      },
+                      child: const Text(
+                        'Entrar',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Text('Ou'),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                  // Botão de cadastro
+                  // Botão com linhas de borda
+                  SizedBox(
+                    height: 50,
+                    child: OutlinedButton(
+                      //formatação do botão
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18),
+                        ),
+                        side: const BorderSide(
+                          width: 2,
+                          color: Colors.green,
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text('Criar conta'),
+                    ),
                   ),
                 ],
               ),
--- app_feirinha/lib/src/components/custom_text_field.dart
+++ app_feirinha/lib/src/components/custom_text_field.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   // passando dados para ícone e label
   final IconData icon;
   final String label;
 
   // variável para configurar a ocultação da senha
-  final bool isObscure;
+  final bool isSecret;
 
   const CustomTextField(
       {super.key,
       required this.icon,
       required this.label,
-      this.isObscure = false});
+      this.isSecret = false, required TextEditingController controller});
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  // variável para configurar a ocultação da senha
+  bool isObscure = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    isObscure = widget.isSecret;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,14 +39,18 @@ class CustomTextField extends StatelessWidget {
         decoration: InputDecoration(
 
           // passando dados do icone e label
-            prefixIcon: Icon(icon),
-            labelText: label,
+            prefixIcon: Icon(widget.icon),
+            labelText: widget.label,
 
             // --- icone de exibição da senha
-            suffixIcon: isObscure
+            suffixIcon: widget.isSecret
                 ? IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.visibility),
+                    onPressed: () {
+                      setState(() {
+                        isObscure = !isObscure;
+                      });
+                    },
+                    icon: Icon( isObscure ? Icons.visibility : Icons.visibility_off),
                   )
                 : null,
             isDense: true,
